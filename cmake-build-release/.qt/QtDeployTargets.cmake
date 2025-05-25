5b9965598dcae7128900c1e456501d14097c6708
PKTOSE <jungwon0719a@naver.com>
2025-05-25 19:55:43 +0900

    Feat: Qt6를 이용한 GUI 완성

---
+++ cmake-build-release/.qt/QtDeployTargets.cmake
@@ -0,0 +1,3 @@
+set(__QT_DEPLOY_TARGET_file_tag_FILE C:/FILES/CLIonProjects/25-1/file_tag/cmake-build-release/file_tag.exe)
+set(__QT_DEPLOY_TARGET_file_tag_TYPE EXECUTABLE)
+set(__QT_DEPLOY_TARGET_file_tag_RUNTIME_DLLS C:/Qt/6.9.0/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.9.0/mingw_64/bin/Qt6Core.dll)
